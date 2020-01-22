ccdfc02bdd37b647dded9e665327132d9b4e7605
uzchu <spdlqjtop@naver.com>
2020-01-22 19:41:22 +0900

    0122 study

---
+++ preamble.py
@@ -0,0 +1,37 @@
+from IPython.display import set_matplotlib_formats, display
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import mglearn
+from cycler import cycler
+
+# 맷플롯립 한글 폰트 지정
+import matplotlib
+import matplotlib.font_manager as fm
+#matplotlib.rc('font', family='NanumSquare_ac')
+matplotlib.use('Agg')
+#matplotlib.rc('font', family="D2Coding")
+font_location = 'C:/Windows/Fonts/NanumSquare.ttf'
+font_name = fm.FontProperties(fname = font_location).get_name()
+matplotlib.rc('font', family = font_name)
+matplotlib.rcParams['axes.unicode_minus'] = False
+
+set_matplotlib_formats('pdf', 'png')
+plt.rcParams['savefig.dpi'] = 300
+plt.rcParams['figure.dpi'] = 300
+plt.rcParams['image.cmap'] = "viridis"
+plt.rcParams['image.interpolation'] = "none"
+plt.rcParams['savefig.bbox'] = "tight"
+plt.rcParams['lines.linewidth'] = 2
+plt.rcParams['legend.numpoints'] = 1
+plt.rc('axes', prop_cycle=(
+    cycler('color', mglearn.plot_helpers.cm_cycle.colors) +
+    cycler('linestyle', ['-', '-', "--", (0, (3, 3)), (0, (1.5, 1.5))])))
+
+
+np.set_printoptions(precision=3, suppress=True)
+
+pd.set_option("display.max_columns", 8)
+pd.set_option('precision', 2)
+
+__all__ = ['np', 'mglearn', 'display', 'plt', 'pd']
